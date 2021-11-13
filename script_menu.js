b4405804953838f5371e266308cd2a61df22fc5e
Lu091156 <luzilima2762@gmail.com>
2021-11-13 16:55:09 -0300

    up

---
+++ script_menu.js
@@ -0,0 +1,15 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded' ,active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'fechar Menu');
+
+    }else {
+        event.currentTarget.setAttribute('aria-label', 'abrir Menu');
+    }
+}
